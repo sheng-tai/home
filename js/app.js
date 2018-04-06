a52dba7a55cd9f877c6633bd8a6666b6258e78e0
SHENG-TAI-CHEN <ezfbk5806949@gmail.com>
2018-04-06 16:33:30 +0800

    erroe bug

--- js/app.js
+++ js/app.js
@@ -1,1 +1,87 @@
-$(function(){var n=!0,t=$("#icon_bar");$("#menu").click(function(){$("#nav").fadeToggle(),n?(t.attr({"xlink:href":"./img/sprite.svg#icon-cross"}),n=!1):(t.attr({"xlink:href":"./img/sprite.svg#icon-menu"}),n=!0)}),$(".navigation__link").click(function(){$("#nav").hide(),t.attr({"xlink:href":"./img/sprite.svg#icon-menu"}),n=!0});var e=0,i=setInterval(function(){$("#val").css("width",e+"%"),(e+=1)>100&&(clearInterval(i),$("#loading").addClass("complete"),setTimeout(function(){$("#loading").hide()},1e3))},30);$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(n){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var t=$(this.hash);(t=t.length?t:$("[name="+this.hash.slice(1)+"]")).length&&(n.preventDefault(),$("html, body").animate({scrollTop:t.offset().top},1e3,function(){var n=$(t);if(n.focus(),n.is(":focus"))return!1;n.attr("tabindex","-1"),n.focus()}))}})});
+$(function () {
+  // menu打開的時候change變為false
+  // 關閉時true 
+  let change = true;
+  const bar = $("#icon_bar");
+  $('#menu').click(function () {
+    $('#nav').fadeToggle()
+
+    if (change) {
+      bar.attr({
+        "xlink:href": "./img/sprite.svg#icon-cross"
+      });
+      change = false
+    } else {
+      bar.attr({
+        "xlink:href": "./img/sprite.svg#icon-menu"
+      });
+      change = true
+    }
+
+    // xlink:href="./img/sprite.svg#icon-menu"
+    // "xlink:href": "./img/sprite.svg#icon-cross"
+    // "xlink:href": "./img/sprite.svg#icon-menu"
+  })
+
+  $('.navigation__link').click(function () {
+    $('#nav').hide()
+    bar.attr({
+      "xlink:href": "./img/sprite.svg#icon-menu"
+    });
+    change = true
+  })
+
+  var percent = 0
+
+  var timer = setInterval(function(){
+    
+    $('#val').css('width', percent+'%') // loading bar 的增加
+    percent += 1
+
+    if(percent > 100) {
+      clearInterval(timer)
+      $('#loading').addClass('complete')  // loadingbar 100width 
+      setTimeout(function() {
+        $('#loading').hide()  // 延遲1秒後消失
+      }, 1000)
+    }
+  },30)
+
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      // On-page links
+      if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $("html, body").animate({
+              scrollTop: target.offset().top
+            },
+            1000,
+            function () {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) {
+                // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }
+            }
+          );
+        }
+      }
+    });
+
+
+})
